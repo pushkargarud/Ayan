f7a57f19aa542e1e29ddb1ca25aef4af33585734
pushkargarud <pushkargarud@gmail.com>
2017-01-03 14:31:28 +0000

    Images added

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -26,6 +27,21 @@ namespace WebApplication1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactModel contactModel)
+        {
+            //ViewBag.Message = "Your contact page.";
+            if (MailSent(contactModel))
+            {
+                ViewBag.SendMessage = true;
+            }
+
+            return View();
+        }
+        public bool MailSent(ContactModel model)
+        {
+            return true;
         }
 
         public ActionResult UnderConstruction()
